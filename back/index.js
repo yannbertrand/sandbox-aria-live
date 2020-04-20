6cd6afad3c1ca7c2bebc72e422800a730ae69f01
Yann Bertrand <yann.bertrand@rca.fr>
2020-04-20 22:00:07 +0200

    :sparkles: Implement the reservation server

---
+++ back/index.js
@@ -0,0 +1,36 @@
+const http = require('http')
+const url = require('url')
+
+const trips = [{
+    departure: 'Nantes',
+    arrival: 'Paris',
+    departureDate: new Date(2020, 10, 10, 10, 2, 0),
+    returnDate: new Date(2020, 10, 10, 12, 4, 0)
+}, {
+    departure: 'Paris',
+    arrival: 'Nantes',
+    departureDate: new Date(2020, 10, 11, 17, 17, 0),
+    returnDate: new Date(2020, 10, 11, 19, 20, 0)
+}, {
+    departure: 'Brest',
+    arrival: 'Paris',
+    departureDate: new Date(2020, 10, 10, 10, 20, 0),
+    returnDate: new Date(2020, 10, 10, 15, 40, 0)
+}, {
+    departure: 'Paris',
+    arrival: 'Brest',
+    departureDate: new Date(2020, 10, 11, 14, 47, 0),
+    returnDate: new Date(2020, 10, 11, 19, 20, 0)
+}]
+
+const server = http.createServer((req, res) => {
+    if (url.parse(req.url).pathname === '/error') {
+        res.writeHead(404)
+        return res.end('Page not found')
+    }
+    
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify(trips))
+})
+
+server.listen(8080)
