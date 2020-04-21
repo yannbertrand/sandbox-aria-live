69634e483d559e9d088492475d473ae52472e3c9
Yann Bertrand <yann.bertrand@rca.fr>
2020-04-21 22:02:38 +0200

    :sparkles: Display server info on the page

--- front/script.js
+++ front/script.js
@@ -1,14 +1,23 @@
-const form = document.forms[0]
+const form = document.forms.roadtripForm
+const checkboxGenerate404 = form.querySelector('[name="generateError"]')
 
 form.onsubmit = async (event) => {
     event.preventDefault()
 
     try {
-        const result = await fetchServer()
-        console.log(result)
+        resetList()
+        displayLoaderList()
+        const result = await fetchServer(checkboxGenerate404.checked)
+
+        if (typeof result === 'object') {
+            displayList(result)
+        } else {
+            displayEmptyList()
+        }
     } catch (error) {
         console.log('An error occured while fetching the server')
         console.log(error)
+        displayError(error)
     }
 }
 
@@ -23,3 +32,61 @@ function fetchServer(error = false) {
 }
 
 const trips = document.getElementById('trips')
+
+function resetList() {
+    trips.innerHTML = ''
+}
+
+function displayLoaderList() {
+    const title = document.createElement('h2')
+    title.textContent = 'Loading results...'
+    trips.appendChild(title)
+}
+
+function displayList(results) {
+    resetList()
+    const title = document.createElement('h2')
+    title.textContent = `Found ${results.length} possible trips`
+    trips.appendChild(title)
+
+    for (const result of results) {
+        trips.appendChild(compileItem(result))
+    }
+}
+
+function displayEmptyList() {
+    resetList()
+    const title = document.createElement('h2')
+    title.textContent = 'No trips corresponding'
+    trips.appendChild(title)
+}
+
+function displayError() {
+    resetList()
+
+    const title = document.createElement('h2')
+    title.textContent = 'The server is unavailable'
+    trips.appendChild(title)
+
+    const subtitle = document.createElement('h3')
+    subtitle.textContent = 'Please try again in a few sec'
+    trips.appendChild(subtitle)
+}
+
+function compileItem({ departure, arrival, departureDate, returnDate }) {
+    let item = document.createElement('div')
+    item.classList.add('choice')
+
+    const departureRealDate = new Date(departureDate)
+    const returnRealDate = new Date(returnDate)
+    let description = document.createElement('p')
+    description.textContent = `From ${departure} to ${arrival}. Departure on ${departureRealDate.toLocaleDateString()}, return on ${returnRealDate.toLocaleDateString()}`
+
+    let button = document.createElement('button')
+    button.textContent = 'Choose this trip'
+
+    item.appendChild(description)
+    item.appendChild(button)
+
+    return item
+}
